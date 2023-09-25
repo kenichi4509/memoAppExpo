7c4a05adc540e263ed417ce8f245361ade3d88e9
kenichi4509 <kenichi.y4509@gmail.com>
2023-09-25 11:11:33 +0900

    Create LogInScreen & SignUpScreen

--- App.jsx
+++ App.jsx
@@ -2,8 +2,10 @@ import React from "react";
 // import MemoListScreen from "./src/screens/MemoListScreen";
 // import MemoDetailScreen from "./src/screens/MemoDetialScreen";
 // import MemoEditScreen from "./src/screens/MemoEditScreen";
-import MemoCreateScreen from "./src/screens/MemoCreateScreen";
+// import MemoCreateScreen from "./src/screens/MemoCreateScreen";
+// import MemoLogInScreen from "./src/screens/MemoLogInScreen";
+import MemoSignUpScreen from "./src/screens/MemoSignUpScreen";
 
 export default function App() {
-  return <MemoCreateScreen />;
+  return <MemoSignUpScreen />;
 }
---
+++ src/screens/MemoLogInScreen.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { View, Text, TextInput, StyleSheet } from "react-native";
+
+import AppBar from "../components/AppBar";
+import Button from "../components/Button";
+export default function MemoLogInScreen() {
+  return (
+    <View style={styles.container}>
+      <AppBar />
+      <View style={styles.inner}>
+        <Text style={styles.title}>Log In</Text>
+        <TextInput style={styles.input} value="Email Address" />
+        <TextInput style={styles.input} value="Password" />
+        <Button label="Submit" />
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>Not registered?</Text>
+          <Text style={styles.footerLink}>Sign up here!</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F0F4F8",
+  },
+  inner: {
+    paddingHorizontal: 27,
+    paddingVertical: 24,
+  },
+  title: {
+    fontSize: 24,
+    lineHeight: 32,
+    fontWeight: "bold",
+    marginBottom: 24,
+  },
+  input: {
+    height: 48,
+    fontSize: 16,
+    lineHeight: 32,
+    borderColor: "#DDD",
+    borderWidth: 1,
+    backgroundColor: "#fff",
+    paddingHorizontal: 8,
+    marginBottom: 16,
+  },
+
+  footerText: {
+    fontSize: 14,
+    lineHeight: 24,
+    marginRight: 8,
+  },
+  footerLink: {
+    fontSize: 14,
+    lineHeight: 24,
+    color: "#467FD3",
+  },
+  footer: {
+    flexDirection: "row",
+  },
+});
